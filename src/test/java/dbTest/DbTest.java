1137ff9f8264415fb9cd19f9428969326080f9ec
Hyojin <1114lhj@gmail.com>
2018-11-18 23:31:28 +0900

    인스턴스 새로 만듦

--- src/test/java/dbTest/DbTest.java
+++ src/test/java/dbTest/DbTest.java
@@ -1,17 +1,26 @@
 package dbTest;
 
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import org.junit.Test;
 
-public class DbTest{
-@Test
-public void test() throws Exception {
-    Class.forName("org.mariadb.jdbc.Driver"); // 마리아DB
-    // Class.forName("com.mysql.jdbc.Driver"); MySQL
-
-    Connection con = DriverManager.getConnection("jdbc:mariadb://192.168.0.141:3306/copynpaste", "copyNpaste", "1004");// 마리아DB
-    // Connection con =     DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/test","root","passwd"); MySQL
-    System.out.println(con);
+public class DbTest {
+    private static final String DRIVER = "org.mariadb.jdbc.Driver";
+    private static final String URL = "jdbc:mariadb://copynpaste.cioylf9z72dx.ap-northeast-2.rds.amazonaws.com:3306/copynpaste";
+    private static final String USER = "copyNpaste";
+    private static final String PASSWORD = "bit1091004";
+    
+    @Test
+    public void testConnection() throws Exception {
+        Class.forName(DRIVER);
+        
+        try(Connection con =  DriverManager.getConnection(URL, USER,  PASSWORD)) {
+            System.out.println("성공");
+            System.out.println(con);
+        }catch(Exception e) {
+            System.out.println("에러발생");
+            e.printStackTrace();
+        }
     }
 }
